632f01537b6a571e5833b017ea16ce1bebc29e2e
simon-2006 <341037@student.mboutrecht.nl>
2025-11-27 13:44:08 +0100

    heel opdracht 3

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+
+        $user = Auth::user();
+        if (!$user) {
+            return redirect()->route('login');
+        }
+
+        $userRole = strtolower($user->rolename ?? '');
+
+        if (!in_array($userRole, $roles, true)) {
+            abort(403, 'Onvoldoende rechten.');
+        }
+
+        return $next($request);
+    }
+}
